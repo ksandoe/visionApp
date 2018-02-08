bf34a884abc89830a3aacd473427d25a21484482
ksandoe <ksandoe@csuchico.edu>
2018-02-08 15:34:53 -0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,39 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { VisionPage } from '../pages/vision/vision';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+import { Camera } from '@ionic-native/camera';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAt8wjKO0zriCBnZa7IM09L8hUfMmC7csE',
+  authDomain: 'visionapp-8606f.firebaseapp.com',
+  databaseURL: 'https://visionapp-8606f.firebaseio.com',
+  projectId: 'visionapp-8606f',
+  storageBucket: 'visionapp-8606f.appspot.com',
+  messagingSenderId: '268519562614'
+};
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage, VisionPage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage, VisionPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    Camera
   ]
 })
 export class AppModule {}
